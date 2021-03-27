c6920daa7896ce7ce5cc850a313bd36ab2081ae4
alageev <alageev5610@gmail.com>
2021-03-27 20:21:54 +0200

    Main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,67 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { initData } from './db';
+import { Biathlete } from './types';
 
-const App: FC = () => (
-  <div>
-    <h1 className="title">React!</h1>
-  </div>
-);
+const App: FC = () => {
+  const [name, setName] = useState('');
+  const [biathletes, setBiathletes] = useState(initData);
+
+  const onPlace = () => {
+    setBiathletes([...biathletes].sort((a, b) => a.place - b.place));
+  };
+
+  const onName = () => {
+    setBiathletes([...biathletes].sort((a, b) => (a.name > b.name ? 1 : -1)));
+  };
+
+  const onHits = () => {
+    setBiathletes([...biathletes].sort((a, b) => (a.hits > b.hits ? 1 : -1)));
+  };
+
+  const onRate = () => {
+    setBiathletes([...biathletes].sort((a, b) => a.rateOfFire - b.rateOfFire));
+  };
+
+  return (
+    <div>
+      <h1>Test task!</h1>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <div>
+        <button type="button" onClick={onPlace}>
+          Place
+        </button>
+        <button type="button" onClick={onName}>
+          Name
+        </button>
+        <button type="button" onClick={onHits}>
+          Hits
+        </button>
+        <button type="button" onClick={onRate}>
+          Rate of fire
+        </button>
+      </div>
+      <ul>
+        {biathletes
+          .filter((item) =>
+            item.name.toLowerCase().includes(name.toLowerCase()),
+          )
+          .map((biathlete: Biathlete) => (
+            <li key={biathlete.id}>
+              <div>{biathlete.place}</div>
+              <div>{biathlete.name}</div>
+              <div>
+                <div>Hits: {biathlete.hits}%</div>
+                <div>Rate of fire: {biathlete.rateOfFire} sec</div>
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
