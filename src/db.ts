6f19b8eebdcda25ab55e692a0477795064220f36
alageev <alageev5610@gmail.com>
2021-03-27 20:04:15 +0200

    Create database

---
+++ src/db.ts
@@ -0,0 +1,74 @@
+import { Biathlete } from './types';
+
+export const initData: Biathlete[] = [
+  {
+    id: 1,
+    name: 'Johannes Thingnes Bø',
+    place: 1,
+    hits: 89.6,
+    rateOfFire: 24.9,
+  },
+  {
+    id: 2,
+    name: 'Sturla Holm Lægreid',
+    place: 2,
+    hits: 88.4,
+    rateOfFire: 25.3,
+  },
+  {
+    id: 3,
+    name: 'Quentin Fillon Maillet',
+    place: 3,
+    hits: 92.4,
+    rateOfFire: 29,
+  },
+  {
+    id: 4,
+    name: 'Tarjei Bø',
+    place: 4,
+    hits: 91.2,
+    rateOfFire: 23.5,
+  },
+  {
+    id: 5,
+    name: 'Johannes Dale',
+    place: 5,
+    hits: 90.0,
+    rateOfFire: 28.3,
+  },
+  {
+    id: 6,
+    name: 'Sebastian Samuelsson',
+    place: 6,
+    hits: 92.5,
+    rateOfFire: 27.1,
+  },
+  {
+    id: 7,
+    name: 'Emilien Jacquelin',
+    place: 7,
+    hits: 85.4,
+    rateOfFire: 23.8,
+  },
+  {
+    id: 8,
+    name: 'Lukas Hofer',
+    place: 8,
+    hits: 92.2,
+    rateOfFire: 23.6,
+  },
+  {
+    id: 9,
+    name: 'Simon Desthieux',
+    place: 9,
+    hits: 90.2,
+    rateOfFire: 26.5,
+  },
+  {
+    id: 10,
+    name: 'Martin Ponsiluoma',
+    place: 10,
+    hits: 89.5,
+    rateOfFire: 27.2,
+  },
+];
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Biathlete = {
+  id: number;
+  name: string;
+  place: number;
+  hits: number;
+  rateOfFire: number;
+};
